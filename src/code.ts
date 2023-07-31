99c037413e1d7dcd82204b15405b9f83459b4893
kawashima-masayoshi <kawashima.masayoshi@moneyforward.co.jp>
2023-08-01 00:53:37 +0900

    refactor: gpt advise

--- src/code.ts
+++ src/code.ts
@@ -1,52 +1,32 @@
-import {tamtam_default} from './components/plugin/01_tamtam_default';
-import {tamtam_excited} from './components/plugin/02_tamtam_excited';
-import {tamtam_crying} from './components/plugin/03_tamtam_crying';
-import {tamtam_ochobo} from './components/plugin/04_tamtam_ochobo';
-import {tamtam_sour} from './components/plugin/05_tamtam_sour';
+import { tamtam_default } from './components/plugin/01_tamtam_default';
+import { tamtam_excited } from './components/plugin/02_tamtam_excited';
+import { tamtam_crying } from './components/plugin/03_tamtam_crying';
+import { tamtam_ochobo } from './components/plugin/04_tamtam_ochobo';
+import { tamtam_sour } from './components/plugin/05_tamtam_sour';
+
+const tamtamSVGs: { [key: string]: string } = {
+  tamtam_default,
+  tamtam_excited,
+  tamtam_crying,
+  tamtam_ochobo,
+  tamtam_sour,
+};
 
 figma.showUI(__html__);
-figma.ui.resize(300,580);
+figma.ui.resize(300, 610);
 
 figma.ui.onmessage = msg => {
-
   if (msg.type === 'create-tamtam') {
     const nodes: SceneNode[] = [];
-    //SVGの用意
-    let vector;
-    switch(msg.tamtamType) {
-      case 'default':
-        vector = figma.createNodeFromSvg(tamtam_default);
-        break;
-      case 'excited':
-        vector = figma.createNodeFromSvg(tamtam_excited);
-        break;
-      case 'crying':
-        vector = figma.createNodeFromSvg(tamtam_crying);
-        break;
-      case 'ochobo':
-        vector = figma.createNodeFromSvg(tamtam_ochobo);
-        break;
-      case 'sour':
-        vector = figma.createNodeFromSvg(tamtam_sour);
-        break;
-      default:
-        console.log('そんなたむたむはいませんよ');
-    }
 
-    if(vector) {
-      //サイズの設定
-      const sizeNum = parseInt(msg.tamtamSize);
-      vector.resize(sizeNum,sizeNum);
-      //色の設定
-      const rgb = hexToRgb(msg.tamtamColor);
-      let baseLayer = vector.findOne(n => n.name === "baseLayer") as VectorNode;
-      const fills = clone(baseLayer.fills);
-      fills[0].color = {
-        r: rgb.r,
-        g: rgb.g,
-        b: rgb.b,
-      }
-      baseLayer.fills = fills;
+    //SVGの用意
+    const selectedSVG = tamtamSVGs[msg.tamtamType];
+    if (!selectedSVG) {
+      showError('そんなたむたむはいませんよ');
+      return;
+    }
+    const vector = createAndResizeTamtam(selectedSVG, msg.tamtamColor, msg.tamtamSize);
+    if (vector) {
       //ページに挿入
       figma.currentPage.appendChild(vector);
       nodes.push(vector);
@@ -62,14 +42,40 @@ figma.ui.onmessage = msg => {
   }
 };
 
+function createAndResizeTamtam(svg: string, hex: string, size: string) {
+  //SVG画像の用意
+  const vector = figma.createNodeFromSvg(svg);
+  //サイズの設定
+  const sizeNum = parseInt(size);
+  vector.resize(sizeNum, sizeNum);
+  //色の設定
+  const rgb = hexToRgb(hex);
+  let baseLayer = vector.findOne(n => n.name === "baseLayer") as VectorNode;
+  const fills = clone(baseLayer.fills);
+  fills[0].color = {
+    r: rgb.r,
+    g: rgb.g,
+    b: rgb.b,
+  }
+  baseLayer.fills = fills;
+
+  return vector;
+}
+
 //baseLayerの色を変更するためにfillsをクローンする必要がある
 function clone(val: readonly Paint[] | typeof figma.mixed) {
   return JSON.parse(JSON.stringify(val));
 }
+
 //hex→rgbに変換
-function hexToRgb(hex: String) {
-  const r = parseInt(hex.substring(1,3), 16);
-  const g = parseInt(hex.substring(3,5), 16);
-  const b = parseInt(hex.substring(5,7), 16);
+function hexToRgb(hex: string) {
+  const r = parseInt(hex.substring(1, 3), 16);
+  const g = parseInt(hex.substring(3, 5), 16);
+  const b = parseInt(hex.substring(5, 7), 16);
   return { r: r / 255, g: g / 255, b: b / 255 };
+}
+
+//エラー関数
+function showError(message: string) {
+  figma.notify(message);
 }
--- src/ui.ts
+++ src/ui.ts
@@ -3,16 +3,18 @@ import './ui.css';
 window.addEventListener('load', () => {
   const create = document.getElementById('create')!;
   const cancel = document.getElementById('cancel')!;
+  const tamtamTypeInput = document.getElementById('tamtamType') as HTMLSelectElement;
+  const tamtamColorInput = document.getElementById('tamtamColor') as HTMLInputElement;
+  const tamtamSizeInput = document.getElementById('tamtamSize') as HTMLInputElement;
 
   create.onclick = () => {
-    const tamtamType = (<HTMLInputElement>document.getElementById('tamtamType')).value;
-    const tamtamColor = (<HTMLInputElement>document.getElementById('tamtamColor')).value;
-    const tamtamSize = (<HTMLInputElement>document.getElementById('tamtamSize')).value;
+    const tamtamType = tamtamTypeInput.value;
+    const tamtamColor = tamtamColorInput.value;
+    const tamtamSize = tamtamSizeInput.value;
     parent.postMessage({ pluginMessage: { type: 'create-tamtam', tamtamType, tamtamColor, tamtamSize } }, '*')
   }
 
   cancel.onclick = () => {
     parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
   }
-  
 })
